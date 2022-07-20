279ebb3de48fb35bdf2354f9a4e1ae9c4467aa72
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-20 20:48:28 +0530

    confirm

--- EcommerceForArtisans/MyApps/apps.py
+++ EcommerceForArtisans/MyApps/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StoreConfig(AppConfig):
+class MyappsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'store'
+    name = 'MyApps'
