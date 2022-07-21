97dacb1358e4066511b660157fa27b899d82521a
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-21 22:47:17 +0530

    confirm

--- EcommerceForArtisans/MyApps/models.py
+++ EcommerceForArtisans/MyApps/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+# ProductsCategory
+class ProductsCategory(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
--- EcommerceForArtisans/MyApps/views.py
+++ EcommerceForArtisans/MyApps/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# Index Page
+def index(request):
+    return render(request, 'base/index.html')
